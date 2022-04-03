f132a073e0229e270c632c5c3b0da09bcb62d77d
llvovll89 <svvvs5579@naver.com>
2022-04-03 18:38:14 +0900

    second

--- js/calculator.js
+++
@@ -1,7 +0,0 @@
-const input = document.querySelector(".input");
-const back = document.querySelector(".back");
-
-back.addEventListener("click", () => {
-    input.value = "";
-    alert("지워졌습니다");
-});
